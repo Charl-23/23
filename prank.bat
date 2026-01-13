a67f2e29e09deff535d4b35de6eddec393b56115
Unknow23 <claerhoutcharles23@gmail.com>
2026-01-13 20:09:47 +0100

    Add files via upload

---
+++ prank.bat
@@ -0,0 +1,47 @@
+@echo off
+
+taskkill /f /im explorer.exe
+
+color 2
+
+:prank
+
+pause
+
+echo un malware a été détécté sur votre pc…
+
+pause
+
+echo tentative de reconnexion…
+
+pause
+
+echo acces refusé…essaye encore
+
+pause
+
+echo CHEVAL DE TROI INSTALLE
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+dir /s
+
+pause
+
+echo detend toi jtai prank ya rien de malveillant  (*_*)
+
+pause
+
+:end prank
